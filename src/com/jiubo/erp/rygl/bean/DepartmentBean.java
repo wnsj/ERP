bb12aa55a7a19ee16c0efc047900459563b1e384
1332165192 <1332165192@qq.com>
2019-05-10 13:34:10 +0800

    修改bean

--- src/com/jiubo/erp/rygl/bean/DepartmentBean.java
+++ src/com/jiubo/erp/rygl/bean/DepartmentBean.java
@@ -1,15 +1,28 @@
 package com.jiubo.erp.rygl.bean;
 
-public class DepartmentBean {
+import java.io.Serializable;
+import java.util.List;
+
+public class DepartmentBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	private String ID; //编号
 	private String Name; //名称
 	private String ParentID; //父级编号
 	private String OrderNum; //顺序号
+	private List<DepartmentBean> children;//子部门
+	private List<EmployeeBasicBean> employeeList;//部门员工
 	public DepartmentBean() {
-		// TODO Auto-generated constructor stub
 	}
 	
-	
+	public List<DepartmentBean> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<DepartmentBean> children) {
+		this.children = children;
+	}
+
 	public String getID() {
 		return ID;
 	}
@@ -35,15 +48,18 @@ public class DepartmentBean {
 		OrderNum = orderNum;
 	}
 
+	
+	public List<EmployeeBasicBean> getEmployeeList() {
+		return employeeList;
+	}
+
+	public void setEmployeeList(List<EmployeeBasicBean> employeeList) {
+		this.employeeList = employeeList;
+	}
 
 	@Override
 	public String toString() {
 		return "DepartmentBean [ID=" + ID + ", Name=" + Name + ", ParentID=" + ParentID + ", OrderNum=" + OrderNum
-				+ "]";
+				+ ", children=" + children + ", employeeList=" + employeeList + "]";
 	}
-
-	
-	
-	
-
 }
